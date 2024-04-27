50dac0e5e90438cf9f77456ac7df210b9890e16a
maldagar150 <jpmaldagar@gmail.com>
2024-04-27 10:25:45 -0500

    version 5.1

--- inc/head.php
+++ inc/head.php
@@ -1,5 +1,6 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Max inventory</title>
+<link rel="icon" type="image/png" href="./img/icono.png">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 <link rel="stylesheet" href="./css/estilos.css">
