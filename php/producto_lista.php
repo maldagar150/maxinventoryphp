8d94ac7121d27d83100a080d548757e3c0ab34c8
maldagar150 <jpmaldagar@gmail.com>
2024-05-01 21:05:09 -0500

    Version 6.0

--- php/producto_lista.php
+++ php/producto_lista.php
@@ -40,12 +40,11 @@
 		foreach($datos as $rows){
 			$tabla.='
 			<article class="media">
-			<figure class="media-left">
-				<img src="' . (is_file("./img/producto/" . $rows['producto_foto']) ? "./img/producto/" . $rows['producto_foto'] : "./img/producto.png") . '" class="img-fluid" style="width: 100px; height: 100px;" alt="Producto">
-			</figure>';
+				<figure class="media-left">
+					<img src="' . (is_file("./img/producto/" . $rows['producto_foto']) ? "./img/producto/" . $rows['producto_foto'] : "./img/producto.png") . '" class="img-fluid" style="width: 100px; height: 100px;" alt="Producto">
+				</figure>';
 			   $tabla.='</p>
-			        </figure>
-			        <div class="media-content">
+			         <div class="media-content">
 			            <div class="content">
 			              <p>
 			                <strong>'.$contador.' - '.$rows['producto_nombre'].'</strong><br>
